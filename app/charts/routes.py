3e055230f0e0b6380563bb1c4332abf45553ff54
Adam Tait <abu@MacBook-Pro-2.local>
2022-10-26 18:01:43 +0200

    refactor: switch to plotly express from graph objects

--- app/charts/routes.py
+++ app/charts/routes.py
@@ -2,8 +2,8 @@ from flask import Blueprint, render_template
 
 import pandas as pd
 import plotly
-import plotly.graph_objects as go
-# import plotly.express as px
+# import plotly.graph_objects as go
+import plotly.express as px
 import json
 
 
@@ -18,31 +18,33 @@ charts = Blueprint(
 def home():
     """TODO: Add function doc"""
 
-    males = pd.read_excel('datasets/bmi4age-datatables.xlsx', sheet_name=0)
-    # females = pd.read_excel('datasets/bmi4age-datatables.xlsx', sheet_name=1)
+    df = pd.read_excel('datasets/bmi4age-datatables.xlsx', sheet_name=0)
+    # df = pd.read_excel('datasets/bmi4age-datatables.xlsx', sheet_name=1)
 
+    sex = "Males"
     percentiles = [
         '3rd', '5th', '10th', '25th', '50th',
         '75th', '85th', '90th', '95th', '97th'
     ]
-    sex = "Males"
 
-    fig = go.Figure()
-
-    for pcent in percentiles:
-        fig.add_trace(go.Scatter(
-            x=males['Age'],
-            y=males[pcent],
-            mode='lines',
-            name=pcent
-        ))
-
-    fig.update_layout(
-        title=f"BMI for Age in {sex}, 2-20 years",
-        xaxis_title="Age in Months",
-        yaxis_title="BMI",
-        height=700
+    fig = px.line(
+        df,
+        x="Age",
+        y=percentiles,
+        height=600,
+        title=f"BMI for Age in {sex}, 2-20",
+        labels={
+            "Age": "Age in Months",
+            "value": "BMI",
+            "variable": "Percentile"
+        },
+        template="plotly_white"
     )
+
+    fig.update_yaxes(
+        showgrid=True
+    )
+
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
     return render_template("index.html", graphJSON=graphJSON)
