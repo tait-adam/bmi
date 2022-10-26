56865a0296a7db8dd08ee6b0faf63ba97e57b96b
Adam Tait <abu@MacBook-Pro-2.local>
2022-10-26 14:33:49 +0200

    feat: add graphing ability with plotly

--- app/charts/routes.py
+++ app/charts/routes.py
@@ -1,5 +1,11 @@
 from flask import Blueprint, render_template
 
+import pandas as pd
+import plotly
+import plotly.graph_objects as go
+# import plotly.express as px
+import json
+
 
 charts = Blueprint(
     'charts',
@@ -10,5 +16,33 @@ charts = Blueprint(
 
 @charts.route("/", methods=['GET', 'POST'])
 def home():
-    """Homepage"""
-    return render_template("index.html")
+    """TODO: Add function doc"""
+
+    males = pd.read_excel('datasets/bmi4age-datatables.xlsx', sheet_name=0)
+    # females = pd.read_excel('datasets/bmi4age-datatables.xlsx', sheet_name=1)
+
+    percentiles = [
+        '3rd', '5th', '10th', '25th', '50th',
+        '75th', '85th', '90th', '95th', '97th'
+    ]
+    sex = "Males"
+
+    fig = go.Figure()
+
+    for pcent in percentiles:
+        fig.add_trace(go.Scatter(
+            x=males['Age'],
+            y=males[pcent],
+            mode='lines',
+            name=pcent
+        ))
+
+    fig.update_layout(
+        title=f"BMI for Age in {sex}, 2-20 years",
+        xaxis_title="Age in Months",
+        yaxis_title="BMI",
+        height=700
+    )
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template("index.html", graphJSON=graphJSON)
